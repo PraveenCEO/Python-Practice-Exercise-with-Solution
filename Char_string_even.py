62a78d082f028c380e8d9ed409d0ada7689786df
Praveen V <praveenvignesh2001@gmail.com>
2023-07-07 21:21:17 +0530

    Create Char_string_even.py
    Write a program to accept a string from the user and display characters that are present at an even index number.

---
+++ Char_string_even.py
@@ -0,0 +1,6 @@
+# Write a program to accept a string from the user and display characters that are present at an even index number.
+str1=str(input())
+for i in range(len(str1)):
+    if i % 2 == 0:
+        print(str1[i])
+

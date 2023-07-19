70ef5aa95188e41faa83288b14dcd8f42b4865b0
Praveen V <praveenvignesh2001@gmail.com>
2023-07-19 19:03:17 +0530

    first_last_list_same.py

--- first_last_list_same.py
+++ first_last_list_same.py
@@ -1,3 +1,4 @@
+# Write a program to return True if the first and last number of a given list is same. If numbers are different then return False.
 n=input()
 list1=n.split()
 print(list1)
@@ -8,3 +9,8 @@ for i in list1:
     else:
         print("false")
         break
+
+# Output:
+10 20 30 40 10
+['10', '20', '30', '40', '10']
+true

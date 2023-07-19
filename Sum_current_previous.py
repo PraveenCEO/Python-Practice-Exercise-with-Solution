17ded0d2e06b6a8eb7ae3782f9666d853d82af36
Praveen V <praveenvignesh2001@gmail.com>
2023-07-19 18:42:07 +0530

    Sum_current_previous.py

--- Sum_current_previous.py
+++ Sum_current_previous.py
@@ -3,3 +3,15 @@ previous_num=0
 for i in range(1,11):
     print("Current number",i,"previous num",previous_num, "Sum:",(i+previous_num))
     previous_num=i
+
+# Output:
+Current number 1 previous num 0 Sum: 1
+Current number 2 previous num 1 Sum: 3
+Current number 3 previous num 2 Sum: 5
+Current number 4 previous num 3 Sum: 7
+Current number 5 previous num 4 Sum: 9
+Current number 6 previous num 5 Sum: 11
+Current number 7 previous num 6 Sum: 13
+Current number 8 previous num 7 Sum: 15
+Current number 9 previous num 8 Sum: 17
+Current number 10 previous num 9 Sum: 19

b78765786365af8b041ecd07e3fa5fedee703d32
Praveen V <praveenvignesh2001@gmail.com>
2023-07-19 18:40:01 +0530

    Update remove_n_char.py

--- remove_n_char.py
+++ remove_n_char.py
@@ -1,3 +1,4 @@
+# Write a program to remove characters from a string starting from zero up to n and return a new string.
 str1=str(input("Enter any text to remove first n:"))
 x=int(input("Enter the length to remove first n number:"))
 n=str1[x:]

bf0cb74ce826c7c9b17ea137c0bb8ccf975ae9e4
Praveen V <praveenvignesh2001@gmail.com>
2024-04-02 12:52:02 +0530

    count_string.py

---
+++ count_string.py
@@ -0,0 +1,12 @@
+#Exercise: Return the count of a given substring from a string
+str1=input()
+word=input()
+cnt=str1.count(word)
+print("The word",word,"appears",cnt,"times")
+
+'''
+Output:
+hi vhellov hi
+hi
+The word hi appears 2 times
+'''
